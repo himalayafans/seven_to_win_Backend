8714787ed053a29a87da5de6ae870c357b8f453f
daofengy <daofengy@oasistech.co.nz>
2022-10-08 18:15:30 +0800

    完善：截图logo或网址的检查算法

--- SevenToWinBackend/Library/Strategy/LogoStrategyHandler.cs
+++ SevenToWinBackend/Library/Strategy/LogoStrategyHandler.cs
@@ -7,16 +7,34 @@ namespace SevenToWinBackend.Library.Strategy
     /// </summary>
     public class LogoStrategyHandler : BaseStrategyHandler
     {
+        private bool IsEnabled(PlayResult result)
+        {
+            foreach (var parsedResult in result.OcrResponse.ParsedResults)
+            {
+                foreach (var line in parsedResult.TextOverlay.Lines)
+                {
+                    foreach (var word in line.Words)
+                    {
+                        var text = word.WordText;
+                        if (text.ContainsIgnoreCase("himalaya") && text.ContainsIgnoreCase("exchange"))
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+
+            return false;
+        }
         public override void Handle(PlayResult result)
         {
-            var text = result.OcrResponse.ParsedResults.First().ParsedText;
-            if(text.ContainsIgnoreCase("himalaya") && text.ContainsIgnoreCase("exchange"))
+            if (IsEnabled(result))
             {
                 this.Successor?.Handle(result);
             }
             else
             {
-                result.Tips.Add("截图没有喜交所的LOGO");
+                result.Tips.Add("截图没有喜交所的LOGO或网址");
             }
         }
     }
